45a3cd7cac056ce48396bb542f340497f990b623
Jorge Acereda <jacereda@gmail.com>
2019-09-20 00:46:04 +0200

    2.42.0

--- pkgs/tools/graphics/graphviz/default.nix
+++ pkgs/tools/graphics/graphviz/default.nix
@@ -1,5 +1,5 @@
 import ./base.nix rec {
   rev = "stable_release_${version}";
-  version = "2.40.1";
-  sha256 = "1xjqq3g2n6jgwp5xzyvibgrxawlskkpam69fjjz9ksrrjas2qwzj";
- }
+  version = "2.42.0";
+  sha256 = "18b2wnz6xk8hndy7dlr1vn9vziyryyflh747n9966778gmh8bick";
+}
